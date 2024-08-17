c454e244d91b4b6d425e7b83194a89ab8f44aa94
Nicolas Czyruk <nicoczyruk2@gmail.com>
2024-08-17 10:30:00 -0300

    secciones servicios y empleo

--- app/routes.py
+++ app/routes.py
@@ -24,28 +24,32 @@ def about():
 
 @main.route('/services')
 def services():
-    services_data = [
-        {
-            'image': 'testimonials-1.jpg',
-            'name': 'Masaje Relajante',
-            'description': 'Un masaje para aliviar el estrés y mejorar la circulación.',
-            'price': '$50'
-        },
-        {
-            'image': 'testimonials-2.jpg',
-            'name': 'Tratamiento Facial',
-            'description': 'Revitaliza tu piel con nuestros tratamientos faciales.',
-            'price': '$70'
-        },
-        {
-            'image': 'testimonials-3.jpg',
-            'name': 'Pedicura',
-            'description': 'Cuida tus pies con nuestra pedicura profesional.',
-            'price': '$30'
-        }
-    ]
+    services_data = {
+        'Masajes': [
+            {'image': 'antiStress.jpg', 'name': 'Anti-stress', 'description': 'Descripción del servicio...', 'price': '$40'},
+            {'image': 'masajeDescontracturante.jpg', 'name': 'Descontracturantes', 'description': 'Descripción del servicio...', 'price': '$50'},
+            {'image': 'masajeConPiedrasCalientes.jpg', 'name': 'Masajes con piedras calientes', 'description': 'Descripción del servicio...', 'price': '$60'},
+            {'image': 'masajeCirculatorio.jpg', 'name': 'Circulatorios', 'description': 'Descripción del servicio...', 'price': '$55'}
+        ],
+        'Belleza': [
+            {'image': 'liftingPesta.jpg', 'name': 'Lifting de pestaña', 'description': 'Descripción del servicio...', 'price': '$70'},
+            {'image': 'depilacionFacial.jpg', 'name': 'Depilación facial', 'description': 'Descripción del servicio...', 'price': '$30'},
+            {'image': 'bellezaManosYPies.jpg', 'name': 'Belleza de manos y pies', 'description': 'Descripción del servicio...', 'price': '$45'}
+        ],
+        'Tratamientos Faciales': [
+            {'image': 'puntaDiamante.jpg', 'name': 'Punta de Diamante', 'description': 'Descripción del servicio...', 'price': '$80'},
+            {'image': 'limpiezaHidratacion.jpg', 'name': 'Limpieza profunda + Hidratación', 'description': 'Descripción del servicio...', 'price': '$90'},
+            {'image': 'crioFrecuencia.jpg', 'name': 'Crio frecuencia facial', 'description': 'Descripción del servicio...', 'price': '$100'}
+        ],
+        'Tratamientos Corporales': [
+            {'image': 'velaSlim.jpg', 'name': 'VelaSlim', 'description': 'Descripción del servicio...', 'price': '$120'},
+            {'image': 'dermoHealth.jpg', 'name': 'DermoHealth', 'description': 'Descripción del servicio...', 'price': '$110'},
+            {'image': 'crioFrecuenciaCorporal.jpg', 'name': 'Criofrecuencia', 'description': 'Descripción del servicio...', 'price': '$115'},
+            {'image': 'ultraCavitacion.jpg', 'name': 'Ultracavitación', 'description': 'Descripción del servicio...', 'price': '$105'}
+        ]
+    }
 
-    return render_template('services.html', services=services_data)
+    return render_template('services.html', categories=services_data)
 
 @main.route('/news')
 def news():
